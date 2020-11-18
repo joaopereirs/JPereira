a0bf9f31d17587a75d2d8a94c1e03863ff3465cb
joaoqpereira <joao.pereira20@imperial.ac.uk>
2020-11-18 17:19:43 +0000

    nothing

--- main.cpp
+++ main.cpp
@@ -2,28 +2,29 @@
 #include <string>
  
 int main(){
-  int n,curmin,curmax;
-  std::string yn;
-  std::string hl;
-  n=5;
-  curmax=11;
-  curmin=0;
-  std::cout<<"Think of a number between 1 and 10 "<<std::endl;
-  std::cout<<"is "<<n<<" your number?"<<std::endl;
-  std::cin>>yn;
-  for(;yn=="no";){
-    std::cout<<"is the number higher or lower than "<<n<<" [h = higher, l=Lower]"<<std::endl;
-    std::cin>>hl;
-    if(hl=="h"){
-      curmin=n;
-      n=curmin+((curmax-curmin)/2);
-      std::cout<<"is "<<n<<" your number?"<<std::endl;
-    } if(hl=="l"){
-      curmax=n;
-      n=curmax-((curmax-curmin)/2);
-      std::cout<<"is "<<n<<" your number?"<<std::endl;
-    }
-    std::cin>>yn;
-  }
-std::cout<<"Great!"<<std::endl;
+    std::string
+    
+    int n1, n2;
+ 
+    std::cout << "please enter two values" << std::endl;
+    std::cin >> n1 >> n2;
+ 
+    std::cout << "you entered:" << std::endl;
+    std::cout << "n1: " << n1 << std::endl;
+    std::cout << "n2: " << n2 << std::endl;
+ 
+    // we swap the values
+    // in order to do so we need to use
+    // a third temporary variable
+ 
+    int tmp;
+ 
+    tmp = n1;
+    n1 = n2;
+    n2 = tmp;
+ 
+    std::cout << "after swapping the values:" << std::endl;
+    std::cout << "n1: " << n1 << std::endl;
+    std::cout << "n2: " << n2 << std::endl;
+ 
 }
