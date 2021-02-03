22e4f6e66602a7aecda30d6ab2b4a4c884c7c660
joaopereirs <joaomariapereira@hotmail.com>
2021-02-03 16:41:32 +0000

    test

--- main.cpp
+++ main.cpp
@@ -29,10 +29,8 @@ void deallocate_list(ListNode* l){
     }
 }
 
-bool search_ordered_list(list_t e, ListNode* l){
-
-      while(l != NULL && l->val<=e){
-        std::cout << l->val << std::endl;
+bool search_list(list_t e, ListNode* l){
+    while(l != NULL){
         if (l->val==e){
             return true;
         } else {
@@ -40,6 +38,43 @@ bool search_ordered_list(list_t e, ListNode* l){
         }
     }
     return false;
+}
+
+bool replace_list(list_t e, list_t n, ListNode* l, bool all){
+    while(l != NULL){
+        if (l->val==e){
+            if (!all){
+                l->val = n; //replaces (only) the first instance
+                return true;
+            } else {
+                // replaces all the instances
+
+            }
+            return true;
+        } else {
+            l = l->next;
+        }
+    }
+    return false;
+}
+
+void replace_all_instances(list_t e, list_t n, ListNode* l){
+    while (l != NULL){
+        if (l->val == e){
+            l->val = n;
+            l = l->next;
+        }
+        else {
+            l = l->next;
+        }
+    }
+}
+
+void print_list(ListNode* l){
+    while(l != NULL){
+        std::cout << l->val << std::endl;
+        l = l->next;
+    }
 }
 
 int main(){
@@ -58,8 +93,16 @@ int main(){
     }
 
     infile.close();
-    std::cout<<"what value would you like to know if it is in the list?"<<std::endl;
+
+    std::cout<<"original list: "<<std::endl;
+    //to do: print original list, then print final list to see the difference and make sure correct
+
+
+    std::cout<<"what value would you like to replace in the list?"<<std::endl;
     std::cin>>e;
+
+    //for which value?
+
     if (search_ordered_list(e,l)){
         std::cout<<"value is in the list!"<<std::endl;
     } else {
