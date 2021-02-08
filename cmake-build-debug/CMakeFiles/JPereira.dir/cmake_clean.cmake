a668488d48c66468cf068466f4bcb6ac51ce0912
joaopereirs <joaomariapereira@hotmail.com>
2021-02-08 21:42:37 +0000

    test

---
+++ cmake-build-debug/CMakeFiles/JPereira.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JPereira.dir/main.cpp.o"
+  "JPereira"
+  "JPereira.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JPereira.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
